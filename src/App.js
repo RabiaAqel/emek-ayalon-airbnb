95e1a4a6a73ae9493a7e52fbef289b07ff553149
Rabia Abuaqel <rabea.aqel@gmail.com>
2023-09-25 18:16:25 +0300

    change content

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { accordionData } from './utils/content';
 const App = () => {
   return (
     <div>
-      <h1>Emek Ayalon 18 Airbnb</h1>
+      <h4>Emek Ayalon 18 Airbnb</h4>
+      <h6>Welcome to our Airbnb! We wish you a wonderful stay, and please feel free to reach out if you have any questions.</h6>
       <div className="accordion">
         {accordionData.map(({ title, content }, index) => (
           <Accordion key={index} title={title} content={content} />
--- src/utils/content.js
+++ src/utils/content.js
@@ -1,17 +1,11 @@
 export const accordionData = [
   {
-    title: 'Welcome',
-    content: `Welcome to our Airbnb! We are thrilled to have you as our guest and are dedicated to making your stay exceptional.
-     We wish you a wonderful stay, and please feel free to reach out if you have any questions.
-      We are here to accommodate you in any way we can.`
-  },
-  {
-    title: 'Check In & Check Out',
-    content: `Check In at 14:00 and Check Out at 11:00.`
+    title: 'Check-In & Check-Out',
+    content: `Check-In at 14:00 and Check-Out at 11:00.`
   },
   {
     title: 'Wifi',
-    content: `You can find details in welcome frame`
+    content: `You can find details in welcome frame.`
   },
   {
     title: 'House Rules',
@@ -30,31 +24,34 @@ export const accordionData = [
   },
   {
     title: 'How to',
-    content: `Carrefour is your closest supermarket. For Shabbat, Tiv Taam is at Yigal Alon 94.
+    content: `Groceries: Carrefour is your closest supermarket. For Shabbat, Tiv Taam is at Yigal Alon 94.;
 
     Kitchen: Water bar, Coffee, tea, sugar, salt, and pepper are at your disposal. A fridge shelf is reserved for you.`
   },
   {
     title: 'Local Transport Information',
-    content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
-    quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
-    dolor ut sequi minus iste? Quas?`
+    content: `The city is walkable, bike-friendly, and has public transport via Moovit. Taxis are accessible through Gett and Yango. Scooter services include Yango, Lime, and Wind.;
+    On Shabbat, you can find special buses which run for free, follow through Moovit.`
   },
   {
     title: 'Sights To See',
-    content: `Cuisine: Taste diverse foods at Shuk HaCarmel. Try local falafel, hummus, shawarma, and knafeh for dessert.
-
-    Beaches: Unwind at Gordon Beach, or surf at Hilton Beach.
+    content: `
+    Beaches: Unwind at Gordon Beach, or surf at Hilton Beach.;
     
-    Nightlife: Experience Rothschild Boulevard's vibrant scene and the lively Florentin district.
+    Nightlife: Experience Rothschild Boulevard's vibrant scene and the lively Florentin district.;
     
     Culture: Visit the Tel Aviv Museum of Art and explore the historical city of Jaffa.
-    
-    Transport: The city is walkable, bike-friendly, and has public transport via Moovit. Taxis are accessible through Gett and Yango. Scooter services include Yango, Lime, and Wind.
-    On Shabbat, you can find special buses which run for free, follow through Moovit.
-    The red line of light trail goes through all tel aviv and Jaffo,
+
     `
   },
+  {
+    title: 'To Eat',
+    content: `Taste diverse foods at Shuk HaCarmel. Try local falafel, hummus, shawarma, and knafeh for dessert
+      `
+    },
+
+  
+
   {
     title: 'Emergency Information',
     content: `Police: 100, Ambulance: 101, Fire: 102`
