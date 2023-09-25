d204c8af184981fba10eae0d2a8b16f1ba9e6fed
Rabia Abuaqel <rabea.aqel@gmail.com>
2023-09-25 18:22:52 +0300

    font sizing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div>
       <h4>Emek Ayalon 18 Airbnb</h4>
-      <h6>Welcome to our Airbnb! We wish you a wonderful stay, and please feel free to reach out if you have any questions.</h6>
+      <h5>Welcome to our Airbnb! We wish you a wonderful stay, and please feel free to reach out if you have any questions.</h5>
       <div className="accordion">
         {accordionData.map(({ title, content }, index) => (
           <Accordion key={index} title={title} content={content} />
