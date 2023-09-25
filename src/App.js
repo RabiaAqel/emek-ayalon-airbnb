869c592312c6f82d000eb643bbb33aaa5d9ce255
Rabia Abuaqel <rabea.aqel@gmail.com>
2023-09-25 18:30:19 +0300

    font sizing2

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { accordionData } from './utils/content';
 const App = () => {
   return (
     <div>
-      <h4>Emek Ayalon 18 Airbnb</h4>
-      <h5>Welcome to our Airbnb! We wish you a wonderful stay, and please feel free to reach out if you have any questions.</h5>
+      <h3>Emek Ayalon 18 Airbnb</h3>
+      <h4>Welcome to our Airbnb! We wish you a wonderful stay, and please feel free to reach out if you have any questions.</h4>
       <div className="accordion">
         {accordionData.map(({ title, content }, index) => (
           <Accordion key={index} title={title} content={content} />
